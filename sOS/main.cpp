e13dadd2395c7cdf17e4db4ddee0173d976971e9
Shadowpatriot9 <gscovil9@gmail.com>
2025-01-01 12:00:34 -0700

    Initial Commit

---
+++ sOS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sOS
+//
+//  Created by Grayden Scovil on 1/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
