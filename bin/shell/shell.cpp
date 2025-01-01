40bab7e5469b9c13ff43bfe2f143ddce6d5022e2
Shadowpatriot9 <gscovil9@gmail.com>
2025-01-01 15:08:17 -0700

    -intial commit, code from previous iteration of project

---
+++ bin/shell/shell.cpp
@@ -0,0 +1,80 @@
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+//
+vector<string> split(const string &input, char delimiter) {
+    vector<string> tokens;
+    stringstream ss(input);
+    string token;
+    
+    while (getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+//
+char ** convert_to_c_array(const vector<string> &args) {
+    char** c_args = new char*[args.size() + 1];
+    for (size_t i = 0; i < args.size(); i++) {
+        c_args[i] = const_cast<char*>(args[i].c_str());
+    }
+    c_args[args.size()] = nullptr;
+    return c_args;
+}
+
+int main() {
+    string input;
+    
+    while (true) {
+        //
+        cout << "myshell> ";
+        getline(cin, input);
+        
+        //
+        if (input == "exit") {
+            break;
+        }
+        
+        //
+        vector<string> args = split(input, ' ');
+        if (args.empty()) {
+            continue;
+        }
+        
+        //
+        char** c_args = convert_to_c_array(args);
+        
+        //
+        pid_t pid = fork();
+        if (pid < 0) {
+            cerr << "Fork Failed" << endl;
+        } else if (pid == 0 ) {
+            //
+            if (execvp(c_args[0], c_args) == -1) {
+                cerr << "Command not found: " << args[0] << endl;
+            }
+            
+            //
+            delete[] c_args;
+            exit(EXIT_FAILURE);
+        } else {
+            //
+            wait(nullptr);
+        }
+        
+        //
+        delete[] c_args;
+        
+    }
+    
+    return 0;
+}
